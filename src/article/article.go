d0732871c683cb6814213ecd07464bb52827de03
Nabeel Sarwar <nabeelsarwar200@gmail.com>
2015-12-30 08:24:47 -0500

    beginning to add comment function to article.go

--- src/article/article.go
+++ src/article/article.go
@@ -1,52 +1,53 @@
 package article
 
-import(
-	"time"
-	_ "text/template" //remove blank identifier to remove unused compiler error
+import (
+	"encoding/json"
 	"io/ioutil"
-  "strings" //remove blank identifier to remove unused compiler error
+	"log"
 	"net/url"
 	"strconv"
-	"encoding/json"
-	"log"
+	"strings"         //remove blank identifier to remove unused compiler error
+	_ "text/template" //remove blank identifier to remove unused compiler error
+	"time"
 )
 
-
-type Article struct{
-	Title string
-	Author string
-	Date time.Time
-	Tags []string
-	Content []byte //content should be template style: see documentation details on golang site
+type Article struct {
+	Title    string
+	Author   string
+	Date     time.Time
+	url      string
+	Tags     []string
+	Content  []byte //content should be template style: see documentation details on golang site
+	Comments []string
 }
-
 
 func SaveJSONArticle(a Article) {
-		b, err := json.Marshal(a)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-		title := strings.ToLower(strings.Replace(a.Title, " ", "_", -1))
-		filePath := "/static/articles" + title
-		// 0644 means overwrite
-		ioutil.WriteFile(filePath, b, 0644)
+	b, err := json.Marshal(a)
+	if err != nil {
+		log.Fatal(err)
 		return
+	}
+	title := strings.ToLower(strings.Replace(a.Title, " ", "_", -1))
+	filePath := "/static/articles" + title
+	// 0644 means overwrite
+	ioutil.WriteFile(filePath, b, 0644)
+	return
 }
+
 /*
 Creates a pointer to an article. Article format must have format above.
 Parsing isn't set up, but it could be done using this format:
 Title:
 Author:
 Date:
-	date
+date
 tags:
-	tag1, tag2, tag3
+tag1, tag2, tag3
 Content:
-	Content
+Content
 Detail to change as no articles are written yet.
 */
-func LoadJSONArticle(title string) (Article, error){
+func LoadJSONArticle(title string) (Article, error) {
 	// must first parse title to find file and then create article out of it
 	filePath := "/static/articles/" + strings.ToLower(strings.Replace(title, " ", "_", -1))
 	b, err := ioutil.ReadFile(filePath)
@@ -67,7 +68,11 @@ func LoadJSONArticle(title string) (Article, error){
 Creates a URL version of an article's title. Signatures use date and title for uniqueness in URL.
 The URL is not totally valid, but the handler will enable its use.
 */
-func (a *Article) parseTitle() string{
+func (a *Article) parseTitle() string {
 	date := "/" + strconv.Itoa(a.Date.Year()) + "/" + strconv.Itoa(int(a.Date.Month())) + "/" + strconv.Itoa(a.Date.Day())
 	return url.QueryEscape(date + "/" + a.Title + ".html")
 }
+
+func (a *Article) addComment(author string, date string, comment string) {
+	var entry = "
+	a.Comments.append(
