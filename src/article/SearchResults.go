331592c1af8eb922d9d6c6cfe1202c3949b3333a
Nabeel Sarwar <nabeelsarwar200@gmail.com>
2016-06-06 14:31:45 -0400

    making search results class

--- src/article/SearchResults.go
+++ src/article/SearchResults.go
@@ -3,7 +3,21 @@ package article
 import (
 	"golang.org/x/text/search"
 	"io/ioutil"
+	"log"
+	"path/filepath"
 )
 
 type SearchResults struct {
+	RelevantArticles []Article
+}
+
+func SearchResults(searchTerms string) SearchResults {
+	var articleDirectory string = filepath.Dir("src/static/article/")
+
+	if articleDirectory == "." {
+		log.Panic("Directory not found")
+	}
+
+	files, err := ioutil.ReadDir(articleDirectory)
+
 }
